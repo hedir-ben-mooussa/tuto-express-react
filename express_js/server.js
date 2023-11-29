8fa6afa80f2ef1c5371c4fe1416f819dbdcbee56
hedir-ben-mooussa <hedirbenmoussa71@gmail.com>
2023-11-29 11:18:11 +0100

    fixed update,try catch

--- express_js/server.js
+++ express_js/server.js
@@ -18,19 +18,38 @@ db.configserverdb
     console.log("err", err);
   });
 
-// app.get("/welcome", (req, res) => {
- // res.status(200).send("welcome to our first backend with expressjs");
-//});
 
+/* GET BY ID */
 app.get("/getbyid/:id", async (req, res) => {
-  const id = req.params.id;
+ 
   try {
+    const id = req.params.id;
     const user = await User.findOne({ where: { id: id } });
     res.status(200).send(user);
   } catch (error) {
     res.status(500).send(`err ${error}`);
   }
 });
+/* ADD */
+app.post("/add", async (req, res) => {
+  try{
+    //destruction of object
+    const { username, email, password } = req.body;
+  return await db.user
+    .create({
+      username: username,
+      email: email,
+      password: password,
+    }),
+    res.status(201).send("user saved");
+  }catch(error){
+    res.status(500).send("server error :", err);
+  }
+  
+  
+   
+});
+/* GET ALL */
 app.get("/getall", async (req, res) => {
   try {
     const users = await User.findAll();
@@ -40,55 +59,47 @@ app.get("/getall", async (req, res) => {
     res.status(500).send(`err ${error}`);
   }
 });
-
+/* UPDATE */
 app.put("/update/:id", async(req, res) => {
-  const id = req.params.id;
-  const user = await User.update({
-username : 'test'
-  },{
-    where : {
-      id : `${id}`
-    }
+  
+  try {
+    const id = req.params.id;
+    let { username, email, password } = req.body;
+    const user = await User.update({
+      username : req.body.username,
+      email:req.body.email,
+      password:req.body.password,
+
+        },{
+          where : {
+            id : id
+          }
+        }
+       )
+       res.status(201).send("user updated");
+
+  }catch(error){
+     res.status(500).send("server error :", err);
   }
- )
- .then(() => {
-  res.status(201).send("user updated");
-})
-.catch((err) => {
-  res.status(500).send("server error :", err);
-});
 });
 
-app.post("/add", async (req, res) => {
-  //destruction of object
-  const { username, email, password } = req.body;
-  return await db.user
-    .create({
-      username: username,
-      email: email,
-      password: password,
-    })
-    .then(() => {
-      res.status(201).send("user saved");
-    })
-    .catch((err) => {
-      res.status(500).send("server error :", err);
-    });
-});
-
+/* DELETE */
 app.delete("/delete/:id", async(req, res) => {
-  const id = req.params.id;
+  try{
+    const id = req.params.id;
   const user = await User.destroy ( {
     where : {
-      id : `${id}`
+      id : id
     }
   })
-  .then(() => {
-    res.status(200).send(`deleted ${id}`);
-  })
-  .catch((err) => {
+  res.status(200).send(`deleted ${id}`);
+  }catch(error){
     res.status(500).send("server error :", err);
-  });
+  }
+  
+  
+    
+  
 });
 
 app.listen(3001, () => {
